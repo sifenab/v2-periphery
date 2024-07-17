f1f7b4f8d8db9b4de77c27b4aa72aea1be33cc04
ziankork <zian@cork.technology>
2024-07-18 01:30:18 +0800

    feat: adjust test

--- test/UniswapV2Router01.spec.ts
+++ test/UniswapV2Router01.spec.ts
@@ -368,8 +368,8 @@ describe('UniswapV2Router{01,02}', () => {
           const receipt = await tx.wait()
           expect(receipt.gasUsed).to.eq(
             {
-              [RouterVersion.UniswapV2Router01]: 101876,
-              [RouterVersion.UniswapV2Router02]: 101898
+              [RouterVersion.UniswapV2Router01]: 102792,
+              [RouterVersion.UniswapV2Router02]: 102836
             }[routerVersion as RouterVersion]
           )
         }).retries(3)
@@ -517,8 +517,8 @@ describe('UniswapV2Router{01,02}', () => {
           const receipt = await tx.wait()
           expect(receipt.gasUsed).to.eq(
             {
-              [RouterVersion.UniswapV2Router01]: 138770,
-              [RouterVersion.UniswapV2Router02]: 138770
+              [RouterVersion.UniswapV2Router01]: 139686,
+              [RouterVersion.UniswapV2Router02]: 139708
             }[routerVersion as RouterVersion]
           )
         }).retries(3)
